2c50217b563d03536d498c82a2b8283a68eec274
amaiable <atmai2002@gmail.com>
2023-09-09 13:14:12 +1000

    Added source file history

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
 import Chatbox from "./components/Chatbox.jsx"
 import Document from "./components/Document.jsx"
+import FileUploader from "./components/FileUploader.jsx"
 
+import { useState, useEffect } from "react"
 
 function App() {
+  const [docTexts, setDocTexts] = useState([])  // Saves all of the document text which has been uploaded so far
+  const [docNames, setDocNames] = useState([])
+  const [showUploader, setShowUploader] = useState(false)  // Toggle open source document upload area
+
+  function handleUpload(text, docName) {
+    setDocTexts(prevDocTexts => [...prevDocTexts, text])
+    setDocNames(prevDocNames => [...prevDocNames, docName])
+  }
+
+  function handleClickUpload() {
+    setShowUploader(!showUploader)
+  }
+
+  useEffect(() => {
+    console.log(docTexts)
+    console.log(docNames)
+  }, [docTexts])
+
   return (
     <>
       <div className="container">
@@ -11,6 +31,13 @@ function App() {
           <Document />
         </div>
         <div className="side-div">
+          {/*<button onClick={handleClickUpload}>Upload Text File</button>
+          {showUploader && <FileUploader onUpload={handleUpload} />}*/}
+          <FileUploader onUpload={(text, name) => handleUpload(text, name)} />
+          <button>Submit</button>
+          <div style={{ marginLeft: '10px', display: 'flex', gap: '10px' }}>
+            {docNames.map((name, index) => <span key={index}>{name}</span>)}
+          </div>
           <Chatbox />
         </div>
       </div>
