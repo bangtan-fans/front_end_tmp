54566e4becda00b23fa1117da3c46d658b073d37
amaiable <atmai2002@gmail.com>
2023-09-09 15:09:48 +1000

    Chat history works, have some temporary data in right now

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Chatbox from "./components/Chatbox.jsx"
 import Document from "./components/Document.jsx"
 import FileUploader from "./components/FileUploader.jsx"
+import ChatApp from "./components/ChatApp.jsx"
 
 import { useState, useEffect } from "react"
 
@@ -19,11 +20,6 @@ function App() {
     setShowUploader(!showUploader)
   }
 
-  useEffect(() => {
-    console.log(docTexts)
-    console.log(docNames)
-  }, [docTexts])
-
   return (
     <>
       <div className="container">
@@ -33,11 +29,12 @@ function App() {
         <div className="side-div">
           {/*<button onClick={handleClickUpload}>Upload Text File</button>
           {showUploader && <FileUploader onUpload={handleUpload} />}*/}
-          <FileUploader onUpload={(text, name) => handleUpload(text, name)} />
+          {/*<FileUploader onUpload={(text, name) => handleUpload(text, name)} />
           <div style={{ marginLeft: '10px', display: 'flex', gap: '10px' }}>
             {docNames.map((name, index) => <span key={index}>{name}</span>)}
-          </div>
-          <Chatbox />
+          </div>*/}
+          {/*<Chatbox />*/}
+          <ChatApp />
         </div>
       </div>
     </>
