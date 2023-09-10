edde4b60667ade18c41bcd15418a8c1be8e26abe
amaiable <atmai2002@gmail.com>
2023-09-10 15:10:15 +1000

    OCR works with images

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -1,23 +1,37 @@
 import SourceDocument from "./SourceDocument"
+import Tesseract from "tesseract.js"
+import React, { useEffect, useState } from "react"
  
 // var text;
 
 function FileUploader({ onUpload, handleFileUpload }) {
+  const [recognizedText, setRecognizedText] = useState("");
+
   function handleFileChange(e) {
     const file = e.target.files[0]
     if (file) {
-      const reader = new FileReader();
-      reader.onload = (function(event) {
-        const text = event.target.result
-        onUpload(text, file.name)
-        //console.log(text)
-        handleFileUpload(file.name.slice(0, -4), text)
-      })
-      reader.readAsText(file)
-      //console.log(file.name.slice(0,-4))
+      console.log(file.name, file.name.slice(-4))
+      if (file.name.slice(-4) === ".txt") {  // Original functionality, only for txt files
+        const reader = new FileReader();
+        reader.onload = (function(event) {
+          const text = event.target.result
+          onUpload(text, file.name)
+          //console.log(text)
+          handleFileUpload(file.name.slice(0, -4), text)
+        })  
+        reader.readAsText(file)
+      }
+      else {
+        async function recogniseText() {
+          if (file) {
+            const result = await Tesseract.recognize(URL.createObjectURL(file))
+            handleFileUpload(file.name.split('.').slice(0, -1).join('.'), result.data.text)
+          }
+        }
+        recogniseText()
+      }
     }
   }
-  //console.log(text, filename)
   return (
     <>
       <input type="file" onChange={handleFileChange} />
